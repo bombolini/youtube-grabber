b5f6b18b648540a80226b4e23266522904158e20
bombolini <43502101+bombolini@users.noreply.github.com>
2022-05-09 17:02:14 +0300

    Update compare.sh

--- compare.sh
+++ compare.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
-while read -r C1 C2
+rm db.sql
+mkdir old
+while read C1 C2
 do
-cp $C1 control_$C1
-./$C2
-new=`cat $C1`
-old=`cat control_$C1`
-if [ ! $new = $old ]
+new=`cat "$C1"_id`
+old=`cat "old/$C1"_id`
+echo "new parameter is $new"
+sleep 1
+echo "old parameter is "$old""
+sleep 1
+if [ ! "$new" = "$old" ]
 then
-mv $C1 control_$C1
+echo "warning! url changed in youtube channel"
+echo "update streams set stream_source = replace ( stream_source, '$old', '$new' ) where id = $C2 ;" >> db.sql
+sleep 3
+mv "$C1"_id old/"$C1"_id
 fi
 done < db
+sed -i '1iuse $enter_database_name_here;\' db.sql
